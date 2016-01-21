b299a5006776890f4d37fe46ebfe9eb28e088ff7
Jianfeng <ginfungchan@gmail.com>
2016-01-20 21:54:10 -0500

    testing py git

---
+++ CLIFF.py
@@ -0,0 +1,12 @@
+__author__ = "Jianfeng Chen"
+__copyright__ = "Copyright (C) 2016 Jianfeng Chen"
+__license__ = "GPL"
+__version__ = "1.0"
+__email__ = "jchen37@ncsu.edu"
+
+"""
+CLIFF algorithm
+Reference: Peters, Fayola, et al. "Balancing privacy and utility in cross-company defect prediction."
+Software Engineering, IEEE Transactions on 39.8 (2013): 1054-1068.
+"""
+
