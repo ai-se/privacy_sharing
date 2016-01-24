bc2e69a57d44ee9af3223deae3a78b578a221162
Jianfeng <ginfungchan@gmail.com>
2016-01-24 16:37:38 -0500

    add morph v1.0

--- MORPH.py
+++ MORPH.py
@@ -1,5 +1,5 @@
 from __future__ import division
-import csv, copy, re, math
+import csv, random, math
 import pdb
 
 
@@ -55,18 +55,87 @@ def attr_norm(all_elements):
         return (element-m)/(M-m) if M != m else 1
 
     def denorm(element):
-         return element*(M-m)+m if M != m else m
+        s = element*(M-m)+m if M != m else m
+        return max(min(s, M), m)  # TODO has better solution for privacy preserving?
 
     return norm, denorm
 
 
+def _str2num(s):
+    try:
+        s = int(s)
+    except ValueError:
+        s = float(s)
+    return s
+
+
+def MOROH(database, writeout=False, alpha=0.15, beta=0.35):
+    # load the database
+    with open('Cliffout/'+database+'.csv', 'r') as db:
+        reader = csv.reader(db)
+        attributes = next(reader)  # including the last one--class tag
+        dataset = []
+        for line in reader:
+            dataset.append(line)
+    dataset = [map(_str2num, row) for row in dataset]  # str to numeric
+    is_int = [type(i) is int for i in dataset[0]]  # save. for better representation of the output table
+    classes = [row[-1] for row in dataset]  # fetch the classes
+    dataset = [row[:-1] for row in dataset]  # separating the raw data and class
+
+    '''dataset transposed mode begins...'''
+    dataset = map(list, zip(*dataset))  # transpose.
+    norm_funcs = []
+    denorm_funcs = []
+
+    # normalizing
+    for attr_index, attr_elements in enumerate(dataset):  # for each attribute elements
+        f1, f2 = attr_norm(attr_elements)
+        norm_funcs.append(f1)
+        denorm_funcs.append(f2)
+        dataset[attr_index] = map(f1, attr_elements)
+
+    '''dataset mode recover...'''
+    dataset = map(list, zip(*dataset))  # transpose again.
+
+    for row_index, row in enumerate(dataset):  # for each row
+        heterogeneous_index = [i for i in range(len(classes)) if classes[i] != classes[row_index]]
+        boundary_dist = min([eulerianDist(row, dataset[heg]) for heg in heterogeneous_index])
+        boundary_dist /= math.sqrt(len(attributes)-1)
+        for i in range(len(row)):
+            dataset[row_index][i] += boundary_dist*random.uniform(alpha, beta)*random.choice([1, -1]) # shake
+
+    '''dataset transposed mode begins...'''
+    dataset = map(list, zip(*dataset))  # transpose.
+    for attr_index, attr_elements in enumerate(dataset):  # for each attribute elements
+        dataset[attr_index] = map(denorm_funcs[attr_index], attr_elements)  # scale to the original
+        for i in range(len(dataset[attr_index])):
+            if is_int[attr_index]:
+                dataset[attr_index][i] = int(round(dataset[attr_index][i]))  # rounding when needed
+    morphed = map(list, zip(*dataset))  # recover to the original mode and finish.
+    '''!!MORPH done!!'''
+
+    # writing out and output the results
+    for row_index in range(len(morphed)):
+        morphed[row_index].append(classes[row_index])
+    morphed.insert(0, classes)
+
+    if writeout:
+        with open('MorphOut/'+database+'.csv', 'wb') as f:
+            writer = csv.writer(f)
+            writer.writerows(morphed)
+
+    return morphed
+
+
 def testing():
-    loc = [100,200,100,300]
-    norm_loc, denorm_loc = attr_norm(loc)
-    l1 = map(norm_loc,loc)
-    l2 = map(denorm_loc, l1)
-    print l1
-    print l2
+    # loc = [100,200,100,300]
+    # norm_loc, denorm_loc = attr_norm(loc)
+    # l1 = map(norm_loc,loc)
+    # l2 = map(denorm_loc, l1)
+    # print l1
+    # print l2
+    MOROH("ant-1.3_0.2", writeout=True)
+    pdb.set_trace()
 
 
 if __name__ == "__main__":
